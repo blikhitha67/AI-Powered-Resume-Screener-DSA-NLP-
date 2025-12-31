0373b87f673bda06cd321ac59eb7fedea389acc1
blikhitha67 <s_basina@u.pacific.edu>
2025-12-31 13:32:20 -0800

    Create tfidf_vectorizer.py

---
+++ src/tfidf_vectorizer.py
@@ -0,0 +1,10 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def get_tfidf_vectors(documents):
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(documents)
+    return tfidf_matrix, vectorizer
+
+#How It Works
+#Transforms resumes and job description into TF-IDF vectors
+#Captures keyword importance across documents
